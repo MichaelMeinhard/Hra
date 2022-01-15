85909ed05aaceb4b7db9e1a9e7e4dae0e008e337
MichaelMeinhard <tank926@seznam.cz>
2022-01-15 10:48:38 +0100

    ShrekJede

--- sketch.js
+++ sketch.js
@@ -1,7 +1,55 @@
+let canvas, shrek, imgS, pozadi;
+
+/*this.gravity = 1;
+this.velocity = 0;
+
+this.update = function(){
+  this.velocity += this.gravity;
+  this.y += this.velocity;
+  }
+*/
+
+function preload() {
+  imgS = loadImage("./img/shrek.png");
+  pozadi = loadImage("./img/pozadi.jpg");
+}
+
+
+class Shrek{
+  constructor(x, y){
+    this.x = x;
+    this.y = width/2;
+  }
+  draw(){
+    push();
+    //square(80, this.y /2 + 25, 50);
+    //noFill();
+    image(imgS.get(0,0, 50, 50), 80, this.y / 2 + 25);
+    pop();
+  }
+
+  update(){
+  this.gravity = 1;
+  this.velocity = 0;
+  this.velocity += this.gravity;
+  this.y += this.velocity;
+  }
+}
+
+function centerCanvas() {
+  var x = (windowWidth - width) / 2;
+  var y = (windowHeight - height) / 2;
+  canvas.position(x, y);
+}
+
 function setup() {
-  createCanvas(400, 400);
+  canvas = createCanvas(1100, 650);
+  canvas.parent("canvas");
+  shrek = new Shrek(80, this.y / 2 + 25);
 }
 
 function draw() {
-  background(220);
+  image(pozadi, 0, 0);
+  shrek.update();
+  shrek.draw();
 }
