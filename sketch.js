515329167b856a8d68b5e124ed0dcddc2547d050
MichaelMeinhard <tank926@seznam.cz>
2022-01-16 10:55:59 +0100

    Funkcni

--- sketch.js
+++ sketch.js
@@ -1,7 +1,6 @@
-let canvas, shrek, imgS, pozadi, wc, score;
+let canvas, shrek, imgS, pozadi, wc, score, button;
 let wcs = [];
 let value = 0;
-let scoreInterval;
 //let music;
 
 /*this.gravity = 1;
@@ -13,6 +12,7 @@ this.update = function(){
   }
 */
 
+
 function preload() {
   imgS = loadImage("./img/shrek.png");
   pozadi = loadImage("./img/pozadi.jpg");
@@ -21,28 +21,31 @@ function preload() {
   //music = loadSound("./sound/AllStar.mp3");
 }
 
-class Score{
-  constructor(){
+/*classa pro score*/
+class Score {
+  constructor() {
     this.x = 50
     this.y = 50
   }
 
-  draw(){
+  draw() {
     textSize(32);
     text(`Score: ${value}`, 50, 50);
     fill(250)
   }
-
-  count(){
-    value = Math.floor((frameCount - 200)/160);
+/*výpočet score aby seděl*/
+  count() {
+    value = Math.floor((frameCount - 200) / 160);
   }
 }
 
+/*classa shreka*/
 class Shrek {
   constructor(x, y) {
     this.x = x;
     this.y = height / 2;
   }
+  /*robrazení shreka*/
   draw() {
     push();
     //square(80, this.y /2 + 25, 50);
@@ -50,13 +53,15 @@ class Shrek {
     image(imgS.get(0, 0, 50, 50), 80, this.y + 25);
     pop();
   }
-
+  /*gravitace *//*omezení spadnutí/vyletění*/
   update() {
     this.gravity = 1;
     this.velocity = 2;
     this.velocity += this.gravity;
     this.y += this.velocity;
     //console.log(this.y);
+
+    /*omezení spadnutí/vyletění*/
     if (this.y + 65 > height) {
       this.y = height - 65;
     }
@@ -64,39 +69,38 @@ class Shrek {
       this.y = -25;
     }
   }
-
+  /*pohyb shreka nahoru*/
   move() {
     if (keyIsDown(32)) {
       ost.play();
-      this.y -= 15;
+      this.y -= 12;
       //console.log("SPACE")
     }
   }
 }
 
-class Wc{
-  constructor(){
-    this.space = 175;
+/*classa záchodů*/
+class Wc {
+  constructor() {
+    this.space = 200;
     this.top = random(height / 6, 3 / 4 * height);
     this.bot = height - (this.top + this.space);
     this.x = width;
     this.w = 120;
     this.speed = 3;
   }
-
-  draw(){
+  /*vyhreslení záchodů*/
+  draw() {
     push();
-    if(this.top + this.bot > 550){
-
-    }
+    //rect(this.x, 0, this.w, this.top);
     image(wc1, this.x, 0, this.w, this.top);
-    image(wc, this.x, height-this.bot, this.w, this.bot);
-    
+    //rect(this.x, height-this.bot, this.w, this.bot);
+    image(wc, this.x, height - this.bot, this.w, this.bot);
     pop();
   }
-
-  update(){
-    this.x -=this.speed;
+  /*rychlost pohybu záchodů*/
+  update() {
+    this.x -= this.speed;
   }
 }
 
@@ -117,9 +121,8 @@ function centerCanvas() {
 
 function setup() {
   canvas = createCanvas(1100, 650);
-  
+
   score = new Score(50, 50);
-  //music.play();
   canvas.parent("canvas");
   shrek = new Shrek(80, height / 2 + 25);
   wcs.push(new Wc());
@@ -129,20 +132,37 @@ function draw() {
   image(pozadi, 0, 0);
   shrek.move();
   shrek.update();
-  shrek.draw(); 
+  shrek.draw();
 
-  if (frameCount % 160 == 0){
-    wcs.push(new Wc());
+  /* každý 160snímek se udělá záchod*/
+  if (frameCount % 160 == 0) {
+    wcs.push(new Wc()); 
   }
 
-  for (let i= wcs.length -1; i >= 0; i--){
+  /*vykreslování záchdoů*/
+  for (let i = wcs.length - 1; i >= 0; i--) {
     wcs[i].draw();
     wcs[i].update();
-    if (wcs[i].x + 120 < 0){
+    /*mazání z pole už neviděných záchodů*/
+    if (wcs[i].x + 120 < 0) {
       wcs.splice(i, 1);
     }
-  }
+    /*výpis score hráče*/
+    score.draw();
+    setTimeout(score.count, 5000)
+    /* Zde je naznačeno řešení kolizí - musíte zkontrolovat možnou kolizi 
+    horního i spodního obdélníku s obdélníkem Shreka. */
+    if (collideRectRect(wcs[i].x + 20, 0, wcs[i].w, wcs[i].top - 35, shrek.x, shrek.y, 50, 50)
+      || collideRectRect(wcs[i].x + 20, height - wcs[i].bot + 10, wcs[i].w, wcs[i].bot, shrek.x, shrek.y, 50, 50)) {
+      noLoop();
+      textSize(85);
+      fill(200, 30, 30);
+      text("Game Over", width/2 - 250, height/2 - 100, 500, 300);
+      text(`Your Score: ${value}`, width/2 - 280, height/2 + 10, 700, 300);
 
-  score.draw();
-  setTimeout(score.count, 5000)
+      /* Počítá úplně všechny kolize obdélníků*/
+      //console.log('Narazil jsi, Shreku!');
+    }
+  }
+  
 }
